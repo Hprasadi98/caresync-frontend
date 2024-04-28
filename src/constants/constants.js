9b971be9dbfc43a4ba987c397d3fefd834686b3c
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-04-28 23:41:44 +0530

    feat: update name

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 const base =
-  Platform.OS === "android" ? "http://10.10.4.244" : "http://localhost";
+  Platform.OS === "android" ? "http://192.168.8.105" : "http://localhost";
 const port = 4001;
 export const baseUrl = `${base}:${port}/api`;

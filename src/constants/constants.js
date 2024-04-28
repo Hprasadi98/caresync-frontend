e9c2ea5d6530db19634811e584585130e30dd29e
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-04-29 01:27:06 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 const base =
-  Platform.OS === "android" ? "http://192.168.8.105" : "http://localhost";
-const port = 4001;
+  Platform.OS === "android" ? "http://10.0.2.2" : "http://localhost";
+const port = 4000;
 export const baseUrl = `${base}:${port}/api`;

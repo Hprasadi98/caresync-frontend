adb4315719a87ba9802814d98146341761e02130
Sanduni Lakshika <110313255+slakshika2026@users.noreply.github.com>
2024-05-20 01:17:37 +0530

    feat: display the entered data - completed

--- src/screens/AddMedicalIncidentScreen/components/MedicalRecordGrid.js
+++ src/screens/AddMedicalIncidentScreen/components/MedicalRecordGrid.js
@@ -3,20 +3,98 @@ import { View, Text, Pressable, Animated, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import ToochableIconDown from '../../ViewPatientSummaryHome Screen/Components/TouchableIconDown';
 
+// Utility function to format date
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = (`0${date.getMonth() + 1}`).slice(-2);
+  const day = (`0${date.getDate()}`).slice(-2);
+  return `${year}/${month}/${day}`;
+};
+
+const incidentConfig = {
+  TEST: {
+    backgroundColor: '#FEFFE0',
+
+
+    renderContent: (incident) => (
+      <>
+        <View style={styles.subcom}>
+          <View style={[styles.innertile, { backgroundColor: "#FFEBA5" }]}>
+            <Text style={styles.innertext}>{incident.incidentType}</Text>
+          </View>
+          <Text style={styles.subtext}>{incident.testType}</Text>
+        </View>
+        <Text style={styles.date}>{formatDate(incident.date)}</Text>
+        <Text style={styles.provider}>Test Provider: {incident.testProvider}</Text>
+      </>
+    ),
+  },
+  SYMPTOM: {
+    backgroundColor: '#FFE0E0',
+    renderContent: (incident) => (
+      <>
+        <View style={styles.subcom}>
+          <View style={[styles.innertile, { backgroundColor: "#FF9999" }]}>
+            <Text style={styles.innertext}>{incident.incidentType}</Text>
+          </View>
+          <Text style={[styles.subtext, { marginTop: '0%' }]}>{incident.symptomType}</Text>
+        </View>
+        <Text style={[styles.date, {}]}>{formatDate(incident.date)}</Text>
+        <Text style={[styles.provider, { marginTop: '-18%' }]}>Freq: {incident.symptomFrequency}</Text>
+        <Text style={[styles.provider, { marginTop: '-1.3%' }]}>Severity: {incident.severity}/10</Text>
+        <Text style={[styles.provider, { marginTop: '-1.5%' }]}>Duration: {incident.SymptomDuration}</Text>
+        <Text style={[styles.provider, { marginTop: '-1.3%' }]}>Appetite: {incident.appetite}/10</Text>
+        <Text style={[styles.provider, { marginTop: '-1%' }]}>Weight: {incident.weight}kg</Text>
+      </>
+    ),
+  },
+  APPOINTMENT: {
+    backgroundColor: '#E0FFE0',
+    renderContent: (incident) => (
+      <>
+        <View style={styles.subcom}>
+          <View style={[styles.innertile, { backgroundColor: "#99FF99" }]}>
+            <Text style={styles.innertext}>{incident.incidentType}</Text>
+          </View>
+          <Text style={styles.subtext}>Dr.{incident.health_pro_name}</Text>
+        </View>
+        <Text style={styles.date}>{formatDate(incident.date)}</Text>
+        <Text style={styles.provider}>Tel:{incident.health_pro_contact}</Text>
+      </>
+    ),
+  },
+  MEDICATION: {
+    backgroundColor: '#E0E0FF',
+    renderContent: (incident) => (
+      <>
+        <View style={styles.subcom}>
+          <View style={[styles.innertile, { backgroundColor: "#9999FF" }]}>
+            <Text style={styles.innertext}>{incident.incidentType}</Text>
+          </View>
+          <Text style={styles.subtext}>{incident.medi_name}</Text>
+        </View>
+        <Text style={styles.date}>{formatDate(incident.date)}</Text>
+        <Text style={[styles.provider, { marginTop: '-13.5%', marginLeft: '36%' }]}>Dosage: {incident.medi_dosage}</Text>
+        <Text style={[styles.provider, { marginTop: '0', marginLeft: '35%' }]}> {incident.medi_Frequency}</Text>
+      </>
+    ),
+  },
+};
+
 function MedicalRecordGrid({
   recordName,
   date,
   recordDescription,
-  incidents
-  = [],
+  incidents = [],
 }) {
   const [expanded, setExpanded] = useState(false);
   const [contentHeight, setContentHeight] = useState(100); // Initial height
   const heightAnim = useRef(new Animated.Value(100)).current;
 
   const calculateContentHeight = () => {
-    const baseHeight = 90; // Base height without incidents
-    const incidentHeight = incidents.length * 110;
+    const baseHeight = 120; // Base height without incidents
+    const incidentHeight = incidents.length * 120;
     return baseHeight + incidentHeight;
   };
 
@@ -32,7 +110,7 @@ function MedicalRecordGrid({
     } else {
       setContentHeight(100); // Reset to initial height
       Animated.timing(heightAnim, {
-        toValue: 120,
+        toValue: 95,
         duration: 300,
         useNativeDriver: false,
       }).start();
@@ -67,49 +145,29 @@ function MedicalRecordGrid({
         <ToochableIconDown
           onPress={handlePress}
           iconName={expanded ? "up" : "down"}
-          iconSize={30}
+          iconSize={20}
           iconColor="white"
         />
       </View>
       <View>
         <Text style={styles.description}>{recordDescription}</Text>
-
-
       </View>
-
-
-
       {incidents.length > 0 && expanded && (
         <View style={styles.incidentContainer}>
-          {incidents.map((incident, index) => (
-            <View key={index} style={styles.subtile}>
-
-              <View style={styles.subcom}>
-
-                <View style={styles.innertile}>
-
-                  <Text style={styles.innertext}>{incident.incidentType}</Text>
-                </View>
-                <Text style={styles.subtext}>{incident.testType}</Text>
+          {incidents.map((incident, index) => {
+            const config = incidentConfig[incident.incidentType] || {};
+            return (
+              <View key={index} style={[styles.subtile, { backgroundColor: config.backgroundColor }]}>
+                {config.renderContent ? config.renderContent(incident) : <Text>Unknown Incident Type</Text>}
               </View>
-              <Text style={styles.date}>{incident.date}</Text>
-
-            </View>
-
-          ))
-
-          }
+            );
+          })}
           <Pressable style={styles.btn} onPress={handleAddNew}>
             <Text style={styles.btntext}>+ New Incident</Text>
           </Pressable>
-
-
         </View>
-
       )}
-
     </Animated.View>
-
   );
 }
 
@@ -129,10 +187,7 @@ const styles = StyleSheet.create({
     shadowColor: "black",
     shadowOpacity: 0.25,
     textShadowRadius: 8,
-
-
   },
-
   btn: {
     backgroundColor: '#DEFFFB',
     alignItems: "center",
@@ -140,8 +195,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     width: '30%',
     alignSelf: "center",
-    marginTop: '1%'
-
+    marginTop: '1%',
   },
   btntext: {
     color: "#00567D",
@@ -149,7 +203,6 @@ const styles = StyleSheet.create({
     margin: 2,
     fontWeight: "bold",
   },
-
   title: {
     fontWeight: "bold",
     fontSize: 18,
@@ -163,78 +216,74 @@ const styles = StyleSheet.create({
     marginTop: 10,
     fontWeight: "600",
   },
-
   icon: {
     position: "absolute",
     right: 10,
     top: 20,
   },
-
   subtile: {
     width: "90%",
     marginBottom: "2%",
     marginTop: "3%",
     marginLeft: "5%",
     marginRight: "5%",
-    height: 60,
+    height: 97,
     borderRadius: 10,
     elevation: 2,
-    backgroundColor: "#FEFFE0",
     shadowColor: "black",
     shadowOpacity: 0.25,
     textShadowRadius: 8,
   },
-
   innertile: {
     width: "32%",
     marginBottom: "2%",
     marginTop: "2%",
-    marginLeft: "3%",
+    marginLeft: "1%",
     marginRight: "3%",
     height: 22,
     borderRadius: 10,
     elevation: 2,
-    backgroundColor: "#FFEBA5",
+
     shadowColor: "black",
     shadowOpacity: 0.25,
     textShadowRadius: 8,
   },
-
   innertext: {
-    fontSize: 13,
+    fontSize: 14,
     marginTop: 2,
     fontWeight: "bold",
     alignSelf: "center",
   },
-
   subtext: {
     paddingLeft: 2,
     marginRight: "3%",
     fontSize: 13,
     marginTop: "2%",
-    fontWeight: "bold",
+    fontWeight: "800",
   },
   subcom: {
     flexDirection: "row",
-  },
 
+  },
   date: {
-    marginLeft: "8%",
-    fontSize: 13,
-    marginTop: "0.5%",
-    fontWeight: "500"
+    marginLeft: "5%",
+    fontSize: 14,
+    marginTop: "5%",
+    fontWeight: "700",
   },
-
   titleGrid: {
     width: "100%",
-    backgroundColor: "#5c074b",
+    backgroundColor: "#575757",
     marginTop: "4%",
     paddingBottom: "2%",
     borderRadius: 10,
   },
-
   incidentContainer: {
     marginTop: 10, // Adjust as needed
   },
-
+  provider: {
+    marginLeft: '36.5%',
+    fontWeight: '600',
+    marginTop: '-6%',
+  },
 });

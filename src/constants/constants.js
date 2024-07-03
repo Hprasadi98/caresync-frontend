f7ff530255c9179b2e8072d98d5ae74c72f91ae5
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-07-03 18:11:33 +0530

    feat: add srtles

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 const base =
-  Platform.OS === "android" ? "http://192.168.238.160" : "http://localhost";
+  Platform.OS === "android" ? "http://192.168.254.160" : "http://localhost";
 
 const port = 5000;
 
